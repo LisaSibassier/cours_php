7ef8cc99767e303626945a76049a599fa1fcbc17
bycrea <yoan.despert@lapiscine.pro>
2019-03-04 21:35:46 +0100

    commentaires maj

--- login.php
+++ login.php
@@ -5,11 +5,11 @@
 	//print_r($_POST);
 
 	//CONNEXION BASE DE DONNEES MYSQL
-	try
+	try 
 	{
 		$db = new PDO('mysql:host=localhost; dbname=blog_e_commerce; charset=utf8', 'root', 'root');
 	}
-	catch(Exception $e)
+	catch(Exception $e) 
 	{
 		die('Erreur : '.$e->getMessage());
 	}
@@ -22,26 +22,32 @@
 		//On injecte les variables de $_POST dans d'autres variables (Par sécurité)
 		$mail = $_POST['mail'];
 		$pass = $_POST['pass'];
-		
+
+		//On consulte la $db (base de donnée) avec une $req (requête sql)
 		$req = $db->prepare('SELECT id FROM user WHERE mail= :mail');
 		$req->bindParam(':mail', $mail);
 		$req->execute();
 		$checkmail = $req->fetch(); 
 
+		//Si $mail existe dans la $db, $checkmail récupère une id et n'est donc pas "Empty"
 		if (!empty($checkmail)) {
 
+			//On consulte la $db avec une $req (requête sql)
 			$req = $db->prepare('SELECT id FROM user WHERE mail= :mail AND password= :pass');
 			$req->bindParam(':mail', $mail);
 			$req->bindParam(':pass', $pass);
 			$req->execute();
 			$checkpass = $req->fetchAll(); 
-
+			
+			//Si le $mail et le $pass corresponde à l'utilisateur, $checkpass ne sera donc pas vide
 			if (!empty($checkpass)) {
-				$msg_error = "Vous êtes connecté";
+				header("location: index.php");
+			//si le mot de passe ne corrspond pas :
 			} else {
 				$msg_error = "Mot de passe incorrect";
 				$usertmp = $_POST['mail'];
 			}
+		//Si $mail n'est pas dans la base de donnée :
 		} else {
 			$msg_error = "Email utilisateur inconnu";
 		}
